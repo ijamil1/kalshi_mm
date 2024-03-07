d4b6bc05924625ef6cbf763a23d98f10c9417f16
irfanjamil <irfanjamil9@gmail.com>
2024-03-07 17:09:08 -0500

    using otm puts/calls improves the shape and believability of the implied pdf that arises from the  prices

---
+++ options_helpers.py
@@ -0,0 +1,47 @@
+'''
+This cell of code is taken from David Duarte's answer on StackOverflow 
+https://stackoverflow.com/questions/61289020/fast-implied-volatility-calculation-in-python and tweaked very slightly
+'''
+
+from scipy.stats import norm
+N = norm.cdf
+
+def bs_call(S, K, T, r, vol):
+    d1 = (np.log(S/K) + (r + 0.5*vol**2)*T) / (vol*np.sqrt(T))
+    d2 = d1 - vol * np.sqrt(T)
+    return S * norm.cdf(d1) - np.exp(-r * T) * K * norm.cdf(d2)
+
+def bs_put(S, K, T, r, vol):
+    d1 = (np.log(S/K) + (r + 0.5*vol**2)*T) / (vol*np.sqrt(T))
+    d2 = d1 - vol * np.sqrt(T)
+    return np.exp(-r * T) * K * norm.cdf(-d2) - S * norm.cdf(-d1)
+    
+def bs_vega(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    return S * norm.pdf(d1) * np.sqrt(T)
+
+def find_vol_call(target_value, S, K, T, r, *args):
+    MAX_ITERATIONS = 10000
+    PRECISION = 1.0e-5
+    sigma = 0.5
+    for i in range(0, MAX_ITERATIONS):
+        price = bs_call(S, K, T, r, sigma)
+        vega = bs_vega(S, K, T, r, sigma)
+        diff = target_value - price  # our root
+        if (abs(diff) < PRECISION):
+            return sigma
+        sigma = sigma + diff/vega # f(x) / f'(x)
+    return sigma # value wasn't found, return best guess so far
+
+def find_vol_put(target_value, S, K, T, r, *args):
+    MAX_ITERATIONS = 10000
+    PRECISION = 1.0e-5
+    sigma = 0.5
+    for i in range(0, MAX_ITERATIONS):
+        price = bs_put(S, K, T, r, sigma)
+        vega = bs_vega(S, K, T, r, sigma)
+        diff = target_value - price  # our root
+        if (abs(diff) < PRECISION):
+            return sigma
+        sigma = sigma + diff/vega # f(x) / f'(x)
+    return sigma # value wasn't found, return best guess so far
