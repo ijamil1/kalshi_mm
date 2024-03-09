b7b2ab347264120b50d8afb61198403309c19a66
irfanjamil <irfanjamil9@gmail.com>
2024-03-08 19:15:05 -0500

    using Polygon instead of yfinance for options data as yf is a bit unreliable for options

--- options_helpers.py
+++ options_helpers.py
@@ -5,6 +5,8 @@ https://stackoverflow.com/questions/61289020/fast-implied-volatility-calculation
 
 from scipy.stats import norm
 import numpy as np
+import yfinance as yf
+import pandas as pd
 N = norm.cdf
 
 def bs_call(S, K, T, r, vol):
@@ -46,3 +48,44 @@ def find_vol_put(target_value, S, K, T, r, *args):
             return sigma
         sigma = sigma + diff/vega # f(x) / f'(x)
     return sigma # value wasn't found, return best guess so far
+
+def get_delayed_options_data(date):
+    spx = yf.Ticker('^SPX')
+    ndx = yf.Ticker('^NDX')
+
+    spx_calls = spx.option_chain(date.isoformat()).calls
+    spx_puts = spx.option_chain(date.isoformat()).puts
+
+    ndx_calls = ndx.option_chain(date.isoformat()).calls
+    ndx_puts = ndx.option_chain(date.isoformat()).puts
+
+    spx_calls['midprice'] = spx_calls.apply(lambda x: (x['bid'] + x['ask'])/2, axis=1)
+    ndx_calls['midprice'] = ndx_calls.apply(lambda x: (x['bid'] + x['ask'])/2, axis=1)
+    spx_puts['midprice'] = spx_puts.apply(lambda x: (x['bid'] + x['ask'])/2, axis=1)
+    ndx_puts['midprice'] = ndx_puts.apply(lambda x: (x['bid'] + x['ask'])/2, axis=1)
+
+    ndx_calls = ndx_calls[['strike', 'bid', 'midprice', 'ask', 'impliedVolatility', 'inTheMoney']]
+    spx_calls = spx_calls[['strike', 'bid', 'midprice', 'ask', 'impliedVolatility', 'inTheMoney']]
+    ndx_puts = ndx_puts[['strike', 'bid', 'midprice', 'ask', 'impliedVolatility', 'inTheMoney']]
+    spx_puts = spx_puts[['strike', 'bid', 'midprice', 'ask', 'impliedVolatility', 'inTheMoney']]
+
+    ndx_calls['type'] = ndx_calls.apply(lambda x: 'call', axis=1)
+    ndx_puts['type'] = ndx_puts.apply(lambda x: 'put', axis=1)
+    spx_calls['type'] = spx_calls.apply(lambda x: 'call', axis=1)
+    spx_puts['type'] = spx_puts.apply(lambda x: 'put', axis=1)
+
+
+    ndx_options = pd.concat([ndx_calls, ndx_puts], ignore_index=True)
+    spx_options = pd.concat([spx_calls, spx_puts], ignore_index=True)
+
+    ndx_options = ndx_options[(ndx_options['midprice'].isna() == False) & (ndx_options['midprice']>0)]
+
+    spx_options = spx_options[(spx_options['midprice'].isna() == False) & (spx_options['midprice']>0)]
+
+    ndx_itm = ndx_options[ndx_options['inTheMoney']==True].sort_values(by=['strike']).reset_index(drop=True)
+    ndx_otm = ndx_options[ndx_options['inTheMoney']==False].sort_values(by=['strike']).reset_index(drop=True)
+
+    spx_itm = spx_options[spx_options['inTheMoney']==True].sort_values(by=['strike']).reset_index(drop=True)
+    spx_otm = spx_options[spx_options['inTheMoney']==False].sort_values(by=['strike']).reset_index(drop=True)
+
+    return ndx_itm, ndx_otm, spx_itm, spx_otm
