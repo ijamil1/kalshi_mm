a4382a5ad842056a36fff595bdde8788e4011b31
irfanjamil <irfanjamil9@gmail.com>
2024-03-13 20:13:10 -0400

    working on using polygon api data to build implied pdfs; running into issues however

--- options_helpers.py
+++ options_helpers.py
@@ -1,6 +1,6 @@
 '''
-This cell of code is taken from David Duarte's answer on StackOverflow 
-https://stackoverflow.com/questions/61289020/fast-implied-volatility-calculation-in-python and tweaked very slightly
+The functions related to computing implied volatility are sourced from: 
+https://www.codearmo.com/blog/implied-volatility-european-call-python
 '''
 
 from scipy.stats import norm
@@ -9,33 +9,137 @@ import yfinance as yf
 import pandas as pd
 N = norm.cdf
 
-def bs_call(S, K, T, r, vol):
-    d1 = (np.log(S/K) + (r + 0.5*vol**2)*T) / (vol*np.sqrt(T))
-    d2 = d1 - vol * np.sqrt(T)
-    return S * norm.cdf(d1) - np.exp(-r * T) * K * norm.cdf(d2)
+N_prime = norm.pdf
+N = norm.cdf
 
-def bs_put(S, K, T, r, vol):
-    d1 = (np.log(S/K) + (r + 0.5*vol**2)*T) / (vol*np.sqrt(T))
-    d2 = d1 - vol * np.sqrt(T)
-    return np.exp(-r * T) * K * norm.cdf(-d2) - S * norm.cdf(-d1)
+
+def black_scholes_call(S, K, T, r, sigma):
+    '''
+
+    :param S: Asset price
+    :param K: Strike price
+    :param T: Time to maturity
+    :param r: risk-free rate (treasury bills)
+    :param sigma: volatility
+    :return: call price
+    '''
+
+    ###standard black-scholes formula
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    call = S * N(d1) -  N(d2)* K * np.exp(-r * T)
+    return call
+
+def black_scholes_put(S, K, T, r, sigma):
+    '''
+
+    :param S: Asset price
+    :param K: Strike price
+    :param T: Time to maturity
+    :param r: risk-free rate (treasury bills)
+    :param sigma: volatility
+    :return: call price
+    '''
+
+    ###standard black-scholes formula
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    put = -S * N(-d1) +  N(-d2)* K * np.exp(-r * T)
+    return put
+
+
+def vega(S, K, T, r, sigma):
+    '''
+
+    :param S: Asset price
+    :param K: Strike price
+    :param T: Time to Maturity
+    :param r: risk-free rate (treasury bills)
+    :param sigma: volatility
+    :return: partial derivative w.r.t volatility
+    '''
+
+    ### calculating d1 from black scholes
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+
+    #see hull derivatives chapter on greeks for reference
+    vega = S * N_prime(d1) * np.sqrt(T)
+    return vega
+
+def implied_volatility_put(P, S, K, T, r, tol=0.0001,
+                            max_iterations=10000):
+    '''
+
+    :param P: Observed put price
+    :param S: Asset price
+    :param K: Strike Price
+    :param T: Time to Maturity
+    :param r: riskfree rate
+    :param tol: error tolerance in result
+    :param max_iterations: max iterations to update vol
+    :return: implied volatility in percent
+    '''
+
+
+    ### assigning initial volatility estimate for input in Newton_rap procedure
+    sigma = 0.3
     
-def bs_vega(S, K, T, r, sigma):
-    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    return S * norm.pdf(d1) * np.sqrt(T)
+    for i in range(max_iterations):
 
-def find_vol_call(target_value, S, K, T, r, *args):
-    MAX_ITERATIONS = 10000
-    PRECISION = 1.0e-5
-    sigma = 0.5
-    for i in range(0, MAX_ITERATIONS):
-        price = bs_call(S, K, T, r, sigma)
-        vega = bs_vega(S, K, T, r, sigma)
-        diff = target_value - price  # our root
-        if (abs(diff) < PRECISION):
-            return sigma
-        sigma = sigma + diff/vega # f(x) / f'(x)
-    return sigma # value wasn't found, return best guess so far
+        ### calculate difference between blackscholes price and market price with
+        ### iteratively updated volality estimate
+        diff = black_scholes_put(S, K, T, r, sigma) - P
 
+        ###break if difference is less than specified tolerance level
+        if abs(diff) < tol:
+            print(f'found on {i}th iteration')
+            print(f'difference is equal to {diff}')
+            break
+
+        ### use newton rapshon to update the estimate
+        sigma = sigma - diff / vega(S, K, T, r, sigma)
+
+    return sigma
+
+
+def implied_volatility_call(C, S, K, T, r, tol=0.0001,
+                            max_iterations=10000):
+    '''
+
+    :param C: Observed call price
+    :param S: Asset price
+    :param K: Strike Price
+    :param T: Time to Maturity
+    :param r: riskfree rate
+    :param tol: error tolerance in result
+    :param max_iterations: max iterations to update vol
+    :return: implied volatility in percent
+    '''
+
+
+    ### assigning initial volatility estimate for input in Newton_rap procedure
+    sigma = 0.3
+    
+    for i in range(max_iterations):
+
+        ### calculate difference between blackscholes price and market price with
+        ### iteratively updated volality estimate
+        diff = black_scholes_call(S, K, T, r, sigma) - C
+
+        ###break if difference is less than specified tolerance level
+        if abs(diff) < tol:
+            print(f'found on {i}th iteration')
+            print(f'difference is equal to {diff}')
+            break
+
+        ### use newton rapshon to update the estimate
+        sigma = sigma - diff / vega(S, K, T, r, sigma)
+
+    return sigma
+
+    
 def find_vol_put(target_value, S, K, T, r, *args):
     MAX_ITERATIONS = 10000
     PRECISION = 1.0e-5
